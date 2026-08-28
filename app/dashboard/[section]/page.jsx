66d394a6ff5bd703e9f5d8559661fd852ecad207
kawsarNK <mdkawsarnk78@gmail.com>
2026-08-29 02:43:56 +0600

    implement DashboardSectionPage and DashboardPage components for dynamic and static dashboard rendering

--- app/dashboard/[section]/page.jsx
+++ app/dashboard/[section]/page.jsx
@@ -1,2 +1,3 @@
 import { Dashboard } from "@/components/dashboard";
 export default async function DashboardSectionPage({ params }) { const { section } = await params; return <Dashboard section={section}/>; }
+
